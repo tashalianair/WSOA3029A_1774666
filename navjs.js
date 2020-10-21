c3f401da14c56c86f0ab14e32196cfecb8c97833
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-10-21 12:03:22 +0200

    navbar moved to the top and the addition of a widget

--- navjs.js
+++ navjs.js
@@ -2,9 +2,10 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+ navbarLinks.classList.toggle('active')}); 
 
-document.getElementById('go-back').addEventListener('click', () => {history.back(); });
 
-document.getElementById('go-forward').addEventListener('click', e => {window.history.forward(); });
+
+
+
+
